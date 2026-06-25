14860e3c4e7fda5be373d66ec95fcab339130620
patrickKottman <patkottman@gmail.com>
2026-06-25 22:05:29 +0200

    add ado-config schematic to add a default ADO PR template

---
+++ ag-schematics/src/ado-config/index.ts
@@ -0,0 +1,35 @@
+import { strings } from '@angular-devkit/core';
+import {
+  Rule,
+  SchematicContext,
+  Tree,
+  apply,
+  applyTemplates,
+  mergeWith,
+  move,
+  url,
+} from '@angular-devkit/schematics';
+
+interface AdoConfigOptions {}
+
+export function adoConfig(options: AdoConfigOptions): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    const templatePath = '/.azuredevops/pull_request_template.md';
+    if (tree.exists(templatePath)) {
+      context.logger.warn(
+        `${templatePath} already exists. Skipping.`
+      );
+      return tree;
+    }
+
+    const sourceTemplates = url('./files');
+    const sourceParametrizedTemplates = apply(sourceTemplates, [
+      applyTemplates({
+        ...strings,
+        ...options,
+      }),
+      move(''),
+    ]);
+    return mergeWith(sourceParametrizedTemplates)(tree, context);
+  };
+}
