50d1c0c000ae9d3cfc14c2383a25b34e102b0c36
patrickKottman <patkottman@gmail.com>
2026-06-26 10:52:00 +0200

    basic github schematic

---
+++ ag-schematics/src/github-config/index.ts
@@ -0,0 +1,46 @@
+import { strings } from '@angular-devkit/core';
+import {
+  Rule,
+  SchematicContext,
+  Tree,
+  apply,
+  applyTemplates,
+  filter,
+  mergeWith,
+  move,
+  url,
+} from '@angular-devkit/schematics';
+
+interface GithubConfigOptions {
+  defaultBranch: string;
+  nodeVersion: string;
+}
+
+const TARGET_FILES = [
+  '/.github/pull_request_template.md',
+  '/.github/ISSUE_TEMPLATE/bug_report.md',
+  '/.github/ISSUE_TEMPLATE/feature_request.md',
+  '/.github/workflows/ci.yml',
+  '/.github/dependabot.yml',
+];
+
+export function githubConfig(options: GithubConfigOptions): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    for (const filePath of TARGET_FILES) {
+      if (tree.exists(filePath)) {
+        context.logger.warn(`${filePath} already exists. Skipping.`);
+      }
+    }
+
+    const sourceTemplates = url('./files');
+    const sourceParametrizedTemplates = apply(sourceTemplates, [
+      applyTemplates({
+        ...strings,
+        ...options,
+      }),
+      filter(path => !tree.exists(path)),
+      move(''),
+    ]);
+    return mergeWith(sourceParametrizedTemplates)(tree, context);
+  };
+}
